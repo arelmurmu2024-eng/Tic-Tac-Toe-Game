e8d2993699d1dd9797cdd912dec802b414fe8215
arelmurmu2024-eng <arelmurmu2024@gmail.com>
2026-04-24 10:49:37 +0530

    added code to store position after each move

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 //Element initialization
 const btn = document.body.querySelectorAll(".btn");
 
+
+//For next function : -1 means no move is given yet
+let map = [[-1,-1,-1],
+            [-1,-1,-1],
+            [-1,-1,-1]];
+
+//Stores the position of the 1,0 => X,O for declaring winner for finale
+//sign => X - 1, O - 0
+function storePosition(value, sign){
+    let temp = value.split(",");
+
+    //Double for loop
+    for(let i = 0; i < map.length; i++){
+        for(let j = 0; j < map.length; j++){
+            if(i == temp[0] && j == temp[1]){
+                map[i][j] = sign
+            }
+        }
+    }
+
+    console.log(map)
+}
+
 //Used in next function
 function changeTheme(className, btnElement){
     btnElement.setAttribute("class", className);
@@ -14,12 +37,18 @@ btn.forEach((b)=>{
         if(b.innerText === "" && count == 1){
             b.innerText = "X";
             count = 0;
-            changeTheme("btn btn-outline-warning",b);
+            changeTheme("btn btn-warning",b);
+
+            // console.log(b.getAttribute("value"));
+            storePosition(b.getAttribute("value"),1);
         }else{
             if (b.innerText == ""){
                 b.innerText = "0";
                 count = 1;
-                changeTheme("btn btn-outline-danger",b);  
+                changeTheme("btn btn-danger",b);  
+
+                // console.log(b.getAttribute("value"));
+                storePosition(b.getAttribute("value"),0);
             }            
         }
 
