7834c34c6be788b9f951134995c621c39db8fc56
arelmurmu2024-eng <arelmurmu2024@gmail.com>
2026-04-24 10:21:51 +0530

    adjusted the core logic

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ function changeTheme(className, btnElement){
     btnElement.setAttribute("class", className);
 }
 
+let count = 1; //X => true => 1 and O => false => 0
+
 //This changes the X and O after corresponding clicks
 btn.forEach((b)=>{
     b.addEventListener("click", ()=>{
-        if(b.innerText === ""){
+        if(b.innerText === "" && count == 1){
             b.innerText = "X";
+            count = 0;
             changeTheme("btn btn-outline-warning",b);
         }else{
-            if(b.innerText == "O"){
-                b.innerText = "X";
-                changeTheme("btn btn-outline-warning",b);
-            }else{
-                b.innerText = "O";
-                changeTheme("btn btn-outline-danger",b);
-            }
+            if (b.innerText == ""){
+                b.innerText = "0";
+                count = 1;
+                changeTheme("btn btn-outline-danger",b);  
+            }            
         }
 
     });
